6dc18d0b146548ab2b1f070f75f6c8040a74633f
Salah Elsayed <salahelsayed00@outlook.com>
2022-02-12 19:50:42 +0200

    initial upload

--- Models/salah.cs
+++ Models/salah.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 
 namespace Paramdic.Models
 {
-    public class Mediator : ApplicationUser
+    public class salah
     {
-
+        public string shehab { get; set; }
     }
 }
